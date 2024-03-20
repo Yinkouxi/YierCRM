dd4de187ea96f88ad3c778728b680f61c471e46a
yinkouxi <3405176636@qq.com>
2024-03-20 08:36:10 +0800

    feat: 暗黑模式

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -4,9 +4,23 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onBeforeMount } from 'vue'
+
+// 初始化时判断是要切换暗黑模式
+onBeforeMount(() => {
+  const dark = ref<string | null>(localStorage.getItem('dark'))
+  const element = document.querySelector('html') as HTMLElement | null
+  if (element) {
+    if (dark.value == 'dark') {
+      element.className = 'dark'
+    } else {
+      element.className = ''
+    }
+  }
+})
+</script>
 
 <style scoped>
-.app {
-}
+@import 'assets/css/style.less';
 </style>
